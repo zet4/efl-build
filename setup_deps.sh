883f20aaf0fd85d9f1da65b76a70a6308a34d159
Andrii Kroitor <an.kroitor@samsung.com>
2016-10-11 13:16:32 +0300

    Add dlfcn library to setup_deps
    It is needed for Tizen sdk version of EFL

--- setup_deps.sh
+++ setup_deps.sh
@@ -39,6 +39,20 @@ while read pkg; do
     echo "[+] $pkg"
   fi
 done < $LIST
+if [ $BITS -eq 64 ]
+then
+  if [ ! -f "download/mingw-w64-x86_64-dlfcn-1.0.0-2-any.pkg.tar.xz" ]
+  then
+    wget "http://downloads.sourceforge.net/project/msys2/REPOS/MINGW/x86_64/mingw-w64-x86_64-dlfcn-1.0.0-2-any.pkg.tar.xz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fmsys2%2Ffiles%2FREPOS%2FMINGW%2Fx86_64%2F&ts=1476175877&use_mirror=heanet" -q -O download/mingw-w64-x86_64-dlfcn-1.0.0-2-any.pkg.tar.xz
+  fi
+  echo "[+] mingw-w64-x86_64-dlfcn-1.0.0-2-any.pkg.tar.xz"
+else
+  if [ ! -f "download/mingw-w64-i686-dlfcn-1.0.0-2-any.pkg.tar.xz" ]
+  then
+    wget "http://downloads.sourceforge.net/project/msys2/REPOS/MINGW/i686/mingw-w64-i686-dlfcn-1.0.0-2-any.pkg.tar.xz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fmsys2%2Ffiles%2FREPOS%2FMINGW%2Fi686%2F&ts=1476175952&use_mirror=heanet" -q -O download/mingw-w64-i686-dlfcn-1.0.0-2-any.pkg.tar.xz
+  fi
+  echo "[+] mingw-w64-i686-dlfcn-1.0.0-2-any.pkg.tar.xz"
+fi
 echo ""
 
 echo "Unpacking packages into ./opt/"
@@ -51,6 +65,14 @@ then
   echo "[+] $pkg"
 fi
 done < $LIST
+if [ $BITS -eq 64 ]
+then
+  tar -C opt/windows_64 -xf download/mingw-w64-x86_64-dlfcn-1.0.0-2-any.pkg.tar.xz mingw64 --strip-components 1
+  echo "[+] mingw-w64-x86_64-dlfcn-1.0.0-2-any.pkg.tar.xz"
+else
+  tar -C opt/windows_32 -xf download/mingw-w64-i686-dlfcn-1.0.0-2-any.pkg.tar.xz mingw32 --strip-components 1
+  echo "[+] mingw-w64-i686-dlfcn-1.0.0-2-any.pkg.tar.xz"
+fi
 echo ""
 
 echo "Setup finished"
