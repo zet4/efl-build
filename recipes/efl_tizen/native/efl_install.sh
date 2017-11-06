21c8e402b26dd5fdcc1218fa5985fc0916e48738
Vyacheslav Reutskiy <v.reutskiy@samsung.com>
2017-11-06 10:36:40 +0200

    recipes: build native efl for tizen with correct options

--- recipes/efl_tizen/native/efl_install.sh
+++ recipes/efl_tizen/native/efl_install.sh
@@ -5,7 +5,9 @@ PROC_COUNT=$(grep -c ^processor /proc/cpuinfo)
 PREFIX="$PWD/native/efl_tizen"
 cd ./sources/efl_tizen/efl
 
-./autogen.sh --prefix="$PREFIX"
+./autogen.sh --prefix="$PREFIX"  --disable-xim --disable-scim --disable-wayland-text-input --disable-gesture --with-tests=none --disable-tslib \
+--disable-physics --disable-gstreamer1 --with-opengl=es --enable-lua-old --disable-libmount --disable-cxx-bindings --disable-static --disable-fribidi \
+--enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-aba
 
 make clean
 make -j "$PROC_COUNT"
